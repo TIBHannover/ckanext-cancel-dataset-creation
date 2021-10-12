95131652f812c06d8b89beb64f40b0ddc5743d08
pooya.oa <pooya.oladazimi@tib.eu>
2021-10-12 10:08:16 +0200

    config the plugin

--- ckanext/cancel_dataset_creation/plugin.py
+++ ckanext/cancel_dataset_creation/plugin.py
@@ -1,14 +1,23 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
 
 
 class CancelDatasetCreationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'cancel_dataset_creation')
+        toolkit.add_resource('public/statics', 'ckanext-cancel-dataset-creation')
+    
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__) 
+
+
+        return blueprint
