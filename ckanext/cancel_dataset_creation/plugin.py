b5d5f7125b3a8908b927f24b9f52bbed69e42269
pooya.oa <pooya.oladazimi@tib.eu>
2022-07-21 10:18:14 +0200

    added post in blueprint

--- ckanext/cancel_dataset_creation/plugin.py
+++ ckanext/cancel_dataset_creation/plugin.py
@@ -26,7 +26,7 @@ class CancelDatasetCreationPlugin(plugins.SingletonPlugin, DefaultTranslation):
             u'/cancel_dataset_creation/cancel_dataset/<package_id>/<is_draft>',
             u'cancel_dataset',
             BaseController.cancel_dataset,
-            methods=['GET']
+            methods=['GET', 'POST']
             )
         
         blueprint.add_url_rule(
